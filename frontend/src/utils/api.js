82b64f782404da0eb558d635222daa9485f5ced2
Shavon63 <newgenesisnyc@gmail.com>
2023-01-20 19:14:57 -0500

    update to axios

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -11,21 +11,21 @@ const config = {
 
 // send a POST request to the /signup 
 export async function formSubmit(formData) {
-    const { data } = await axios.post('users/signup/', formData)
+    const { data } = await axios.post('/users/signup/', formData)
     return data
 }
 
 // get user data if user is logged in
 export async function logIn(formData) {
     // hit the user show route
-    const { data } = await axios.post('users/login/', formData)
+    const { data } = await axios.post('/users/login/', formData)
     return data
 }
 
 // get user data if user is logged in
 export async function getUserData() {
     // hit the user show route
-    const { data } = await axios.get('users/',  config)
+    const { data } = await axios.get('/users/',  config)
     return data
 }
 
@@ -35,7 +35,7 @@ export async function deleteUsers() {
 }
 
 export async function updateUser(userId, formData){
-    const { data } = await axios.put('users/' + userId, formData)
+    const { data } = await axios.put('/users/' + userId, formData)
 return data
 }
 
