27dbe3039aac9c6aa33138ee1cf3211e7461bb22
Shavon63 <newgenesisnyc@gmail.com>
2023-01-23 03:15:30 -0500

    complete

--- frontend/src/pages/Map/index.js
+++ frontend/src/pages/Map/index.js
@@ -4,18 +4,18 @@ import Places from "../../components/Places"
 
 
 export default function Map(){
-    // // is loaded is a deconstructable variable calling on the api info from 
-    // // google maps using key
-    // const {isLoaded} = useLoadScript({
-    //     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
-    //     libraries: ["places"]
-    // })
-    // // while waiting for information to be loaded 
-    // // loading ... will show 
-    // if(!isLoaded) 
-    // return(
-    //     <h3>Loading...</h3>
-    // )
+    // is loaded is a deconstructable variable calling on the api info from 
+    // google maps using key
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+        libraries: ["places"]
+    })
+    // while waiting for information to be loaded 
+    // loading ... will show 
+    if(!isLoaded) 
+    return(
+        <h3>Loading...</h3>
+    )
 
 
     return (
