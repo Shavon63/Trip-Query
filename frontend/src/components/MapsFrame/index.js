5eef550f019aebd48ac29f2abffe011339879b72
Shavon63 <newgenesisnyc@gmail.com>
2023-01-26 14:36:33 -0500

    last update

--- frontend/src/components/MapsFrame/index.js
+++ frontend/src/components/MapsFrame/index.js
@@ -1,51 +1,51 @@
-import {useState, useMemo, useCallback, useRef} from "react"
-import GoogleMapReact from 'google-map-react'
-import React from "react"
-import Places from "../Places"
-import Distances from "../Distances"
-import "./maps.css"
+// import {useState, useMemo, useCallback, useRef} from "react"
+// import GoogleMapReact from 'google-map-react'
+// import React from "react"
+// import Places from "../Places"
+// import Distances from "../Distances"
+// import "./maps.css"
 
 
-export default function Maps(){
+// export default function Maps(){
 
-// use memo is like a useEffect, it generates a value once and then continue using it unless something changes 
-// if i was to use a useEffect the value would only be useable inside the useEffect.
-    const center = useMemo(()=>({lat: 40.6, lng: -73.5}), []);
-    //to disable the icons on the map that will come up when zooming in
-    const options = useMemo(()=>({
-        // mapId was fetched from google from customized map
-        mapID: "9223f86a903aea2a",
-        disableDefaultUI: true, 
-        clickableIcons: false,
+// // use memo is like a useEffect, it generates a value once and then continue using it unless something changes 
+// // if i was to use a useEffect the value would only be useable inside the useEffect.
+//     const center = useMemo(()=>({lat: 40.6, lng: -73.5}), []);
+//     //to disable the icons on the map that will come up when zooming in
+//     const options = useMemo(()=>({
+//         // mapId was fetched from google from customized map
+//         mapID: "9223f86a903aea2a",
+//         disableDefaultUI: true, 
+//         clickableIcons: false,
 
-    }),[])
+//     }),[])
 
-    // is the reference to the current State of the map useable in a variable 
-    const mapRef = useRef()
-    // react function call back to create a function that can transfer data
-    // once loaded the mapref will show its current state
-    const loaded = useCallback(map => (mapRef.current = map), [])
-    // ability to set a location 
-    const [location, setLocation] = useState()
-    return(
-        <div className="container-map">
-            <div className="controls">
-                <h1 className="commute">commute?</h1>
-                <Places/>
-            </div> 
-            <div className="map">
-                <GoogleMapReact 
-                zoom={10} 
-                center={center} 
-                mapContainerClassName="map-container"
-                options={options}
-                loaded={loaded}
-                >
+//     // is the reference to the current State of the map useable in a variable 
+//     const mapRef = useRef()
+//     // react function call back to create a function that can transfer data
+//     // once loaded the mapref will show its current state
+//     const loaded = useCallback(map => (mapRef.current = map), [])
+//     // ability to set a location 
+//     const [location, setLocation] = useState()
+//     return(
+//         <div className="container-map">
+//             <div className="controls">
+//                 <h1 className="commute">commute?</h1>
+//                 <Places/>
+//             </div> 
+//             <div className="map">
+//                 <GoogleMapReact 
+//                 zoom={10} 
+//                 center={center} 
+//                 mapContainerClassName="map-container"
+//                 options={options}
+//                 loaded={loaded}
+//                 >
             
-                </GoogleMapReact>
-            </div>
+//                 </GoogleMapReact>
+//             </div>
             
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
--- frontend/src/components/Places/index.js
+++ frontend/src/components/Places/index.js
@@ -1,46 +1,46 @@
-import PlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId, getLatLng } from "react-places-autocomplete";
-import {useState} from "react"
-import  GoogleMapReact  from "@react-google-maps/api";
+// import PlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId, getLatLng } from "react-places-autocomplete";
+// import {useState} from "react"
+// import  GoogleMapReact  from "@react-google-maps/api";
 
 
 
-    function Places(){
-        const [address, setAddress] = useState("")
-        const [coordinates, setCoordinates] = useState({
-            lat: null, 
-            lng: null
-        })
+//     function Places(){
+//         const [address, setAddress] = useState("")
+//         const [coordinates, setCoordinates] = useState({
+//             lat: null, 
+//             lng: null
+//         })
     
-        const handleSelect = async value => {
+//         const handleSelect = async value => {
     
             
-            //when clicked this google function will run 
-            // finnding the geocode by address wich is all info on address
-            const results = await geocodeByAddress(value)
-            // when selected this function will run after which will get the latlng 
-            // from the item selected
-            const latlong = await getLatLng(results[0])
-            // console.log(results)
-            // console.log(latlong)
-            //setting the address in usestate to the result/vaule of the results function
-            setAddress(value)
-            //setting the coordinnates the results of latlong function 
-            // just passing in latlong as the new vaule.
-            setCoordinates(latlong)
-        }
+//             //when clicked this google function will run 
+//             // finnding the geocode by address wich is all info on address
+//             const results = await geocodeByAddress(value)
+//             // when selected this function will run after which will get the latlng 
+//             // from the item selected
+//             const latlong = await getLatLng(results[0])
+//             // console.log(results)
+//             // console.log(latlong)
+//             //setting the address in usestate to the result/vaule of the results function
+//             setAddress(value)
+//             //setting the coordinnates the results of latlong function 
+//             // just passing in latlong as the new vaule.
+//             setCoordinates(latlong)
+//         }
     
-        return (
-            <div className="places-container">
-                <p>lat: {coordinates.lat}</p>
+//         return (
+//             <div className="places-container">
+//                 <p>lat: {coordinates.lat}</p>
     
-                <p>lat: {coordinates.lng}</p>
+//                 <p>lat: {coordinates.lng}</p>
     
-                <p>Address: {address}</p>
+//                 <p>Address: {address}</p>
     
-         </div>
-        )}
+//          </div>
+//         )}
 
 
-export default Places;
+// export default Places;
 
 
